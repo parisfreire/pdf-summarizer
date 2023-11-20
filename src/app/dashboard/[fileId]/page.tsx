763248020fa108bacb0c638099ee9cd8fc0720c8
Paris Freire <parisfreiregallego@gmail.com>
2023-11-20 21:45:33 +0000

    Fixing ChatWrapper props

--- src/app/dashboard/[fileId]/page.tsx
+++ src/app/dashboard/[fileId]/page.tsx
@@ -1,35 +1,35 @@
-import {getKindeServerSession} from "@kinde-oss/kinde-auth-nextjs/server";
-import {notFound, redirect} from "next/navigation";
-import {db} from "@/db";
-import PdfRenderer from "@/components/PdfRenderer";
-import ChatWrapper from "@/components/chat/ChatWrapper";
+import ChatWrapper from '@/components/chat/ChatWrapper'
+import PdfRenderer from '@/components/PdfRenderer'
+import { db } from '@/db'
+import { getUserSubscriptionPlan } from '@/lib/stripe'
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
+import { notFound, redirect } from 'next/navigation'
 
 interface PageProps {
     params: {
-        fileId: string
+        fileid: string
     }
 }
 
-const Page = async ({params}: PageProps) => {
-    // retrieve the file id
-    const { fileId } = params
-
-
+const Page = async ({ params }: PageProps) => {
+    const { fileid } = params
 
     const { getUser } = getKindeServerSession()
     const user = await getUser()
 
-    if(!user || !user.id) redirect(`/auth-callback?origin=dashboard/${fileId}`)
+    if (!user || !user.id)
+        redirect(`/auth-callback?origin=dashboard/${fileid}`)
 
-    // make DB call
     const file = await db.file.findFirst({
         where: {
-            id: fileId,
-            userId: user.id
-        }
+            id: fileid,
+            userId: user.id,
+        },
     })
 
-    if(!file) notFound()
+    if (!file) notFound()
+
+    const plan = await getUserSubscriptionPlan()
 
     return (
         <div className='flex-1 justify-between flex flex-col h-[calc(100vh-3.5rem)]'>
@@ -38,12 +38,12 @@ const Page = async ({params}: PageProps) => {
                 <div className='flex-1 xl:flex'>
                     <div className='px-4 py-6 sm:px-6 lg:pl-8 xl:flex-1 xl:pl-6'>
                         {/* Main area */}
-                           <PdfRenderer url={file.url}/>
+                        <PdfRenderer url={file.url} />
                     </div>
                 </div>
 
                 <div className='shrink-0 flex-[0.75] border-t border-gray-200 lg:w-96 lg:border-l lg:border-t-0'>
-                    <ChatWrapper fileId={file.id} />
+                    <ChatWrapper isSubscribed={plan.isSubscribed} fileId={file.id} />
                 </div>
             </div>
         </div>
