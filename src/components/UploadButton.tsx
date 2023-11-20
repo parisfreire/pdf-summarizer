0155c27092c150e61072f8e5fb1b7fca85286b7a
Paris Freire <parisfreiregallego@gmail.com>
2023-11-20 22:26:37 +0000

    Iphone dialog fix

--- src/components/UploadButton.tsx
+++ src/components/UploadButton.tsx
@@ -62,6 +62,11 @@ const UploadDropzone = ({
     return (
         <Dropzone
             multiple={false}
+            accept={
+                {
+                    'application/pdf': []
+                }
+            }
             onDrop={async (acceptedFile) => {
                 setIsUploading(true)
 
@@ -101,7 +106,6 @@ const UploadDropzone = ({
                     className='border h-64 m-4 border-dashed border-gray-300 rounded-lg'>
                     <div className='flex items-center justify-center h-full w-full'>
                         <label
-                            htmlFor='dropzone-file'
                             className='flex flex-col items-center justify-center w-full h-full rounded-lg cursor-pointer bg-gray-50 hover:bg-gray-100'>
                             <div className='flex flex-col items-center justify-center pt-5 pb-6'>
                                 <Cloud className='h-6 w-6 text-zinc-500 mb-2' />
@@ -146,14 +150,11 @@ const UploadDropzone = ({
                                     ) : null}
                                 </div>
                             ) : null}
-
-                            <input
-                                {...getInputProps()}
-                                type='file'
-                                id='dropzone-file'
-                                className='hidden'
-                            />
                         </label>
+                        <input
+                            {...getInputProps()}
+                            id='dropzone-file'
+                        />
                     </div>
                 </div>
             )}
